2912ebf9129ba7643f4450a3cdbf7b33de15bd70
LexyKim <lexy.kim.swg@gmail.com>
2018-02-07 09:15:56 +0300

    Add system info output

---
+++ Lol.py
@@ -0,0 +1,10 @@
+import platform
+
+print('PythonTestGitProject')
+
+
+print(platform.system())
+print(platform.platform())
+print(platform.machine())
+print(platform.version())
+print(platform.processor())
